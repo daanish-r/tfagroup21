f250ce0f113bb4a6f737c058efdbe6f4cdb507aa
Aditya Baser <asb2287@columbia.edu>
2019-11-28 05:14:36 +0000

    Few fields added to models

--- squirrel/models.py
+++ squirrel/models.py
@@ -2,45 +2,108 @@ from django.db import models
 
 from django.utils.translation import gettext as _
 
+#Create your models here
 
-class Pet(models.Model):
-    name = models.CharField(
-            help_text = _('Name of Pet'),
+class Fields(models.Model):
+
+    Unique_Squirrel_ID  = models.CharField(
+            help_text = _('Unique squirrel ID'),
             max_length=100,
+            unique = True
+            primary_key = True
          )
     
+    Specific_Location  = models.CharField(
+            help_text = _('Specific location of squirrel inside central park'),
+            max_length=300,
+         )
 
-    species = models.CharField(
-            help_text=_('Species of animal'),
-            max_length=100,
+    Other_Activities  = models.CharField(
+            help_text = _('Other Activities'),
+            max_length=300,
+         )
+
+    Date = models.DateField(
+            help_text = _('Date when the squirrel was observed'),
         )
 
-
-
-
-    birth_date = models.DateField(
-            help_text = _('Birth Date'),
-        )
-
-    MALE = 'male'
-    FEMALE = 'female'
-    OTHER = 'other'
+    AM = 'am'
+    PM = 'pm'
     
-    SEX_CHOICES = (
-            (MALE, 'Male'),
-            (FEMALE, 'Female'),
-            (OTHER, 'Other'),
+    SHIFT_CHOICES = (
+            (AM, 'am'),
+            (PM, 'pm'),
         )
 
 
-    sex = models.CharField(
-        help_text = _('Sex of Pet'),
-        max_length = 16,
-        choices = SEX_CHOICES,
-        default = OTHER,
-    )
+    SHIFT = models.CharField(
+        help_text = _('shit'),
+        max_length = 2,
+        choices = SHIFT_CHOICES,
+        )
+
+    Juvenile = 'Juvenile'
+    Adult = 'Adult'
+    ? = '?'
+    
+    AGE_CHOICES = (
+            (Juvenile, 'Juvenile'),
+            (Adult, 'Adult'),
+            (?, '?'),
+        )
+
+
+    AGE = models.CharField(
+        help_text = _('Age of the squirrel'),
+        max_length = 100,
+        choices = AGE_CHOICES,
+        null = True
+        )
+
+    Grey = 'Grey'
+    Cinnamon = 'Cinnamon'
+    Black = 'Black'
+    
+    FUR_CHOICES = (
+            (Grey, 'Grey'),
+            (Cinnamon, 'Cinnamon'),
+            (Black, 'Black'),
+        )
+
+
+    PRIMARY_FUR_COLOR = models.CharField(
+        help_text = _('Primary fur color of the squirrel'),
+        max_length = 100,
+        choices = FUR_CHOICES,
+        null = True
+        )
+
+    Above = 'Above'
+    Below = 'Below'
+    
+    LOCATION_CHOICES = (
+            (Above, 'Above'),
+            (Below, 'Below'),
+        )
+
+
+    LOCATION = models.CharField(
+        help_text = _('Locatrion of the squirrel'),
+        max_length = 100,
+        choices = LOCATION_CHOICES,
+        null = True
+        )
+
+
+    Latitude = models.IntegerField(
+            help_text = ('Latitude of the sighting')
+            null=True
+            )
+
+    Longitude = models.IntegerField(
+            help_text = ('Longitude of the sighting')
+            null=True
+            )
 
     def __str__(self):
         return self.name
-
-# Create your models here.
