954b343f6a463b71203d65f2b92d61a0640439d7
adityabaser <53112019+adityabaser@users.noreply.github.com>
2019-11-30 20:13:35 -0500

    initialize urls

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-            path('list/', views.all_pets),
-            path('<int:pet_id/>', views.pet_details),
+            path('', views.index, name='index'),
             ]
