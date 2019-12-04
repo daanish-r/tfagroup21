ee94964700b84e7efa5d99efd98748a8ccc6cfff
daanish <dsr2152@columbia.edu>
2019-12-04 21:31:26 +0000

    views map addition

--- squirrel/views.py
+++ squirrel/views.py
@@ -4,3 +4,23 @@ from .models import Fields
 
 def index(request):
     return HttpResponse("Hello, this app lets you track squirrels and take a note")
+
+
+class SightingForm(ModelForm):
+    class Meta:
+        model = Sighting
+        fields = '__all__'
+
+def sighting_list(request, template_name='squirrel/all.html'):
+    sightings = Sighting.objects.all()
+    context = {
+            'sightings' : sightings,
+    }
+    return render(request, template_name, context)
+
+def sighting_map(request, template_name='squirrel/map.html'):
+    sightings = Sighting.objects.order_by('?')[:100]
+    context = {
+            'sightings' : sightings,
+    }
+    return render(request, template_name, context)
