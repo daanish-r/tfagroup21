ad002416156ce292817a4e803afa835d4b96e8da
daanish-r <56944972+daanish-r@users.noreply.github.com>
2019-12-07 16:48:02 -0500

    Update import_squirrel_data.py

--- squirrel/management/commands/import_squirrel_data.py
+++ squirrel/management/commands/import_squirrel_data.py
@@ -7,43 +7,38 @@ class Command(BaseCommand):
     help = 'Import Squirrel Census Data'
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', nargs='+', type=str, help='The CSV file to be imported')
+        parser.add_argument('csv_file')
 
-    def handle(self,*args, **options):
-
-        for csv_file in options['csv_file']:
-            dataReader = csv.reader(open(csv_file), delimiter=',', quotechar='"')
-            next(dataReader)
-
-            for row in dataReader:
+    def handle(self, *args, **options):
+        with open(options['csv_file']) as fp:
+            reader = csv.DictReader(fp)
+            data = list(reader)
+        for item in data:
                 F = Fields()
-
                 try:
-                    F.longitude = row[0]
-                    F.latitude = row[1]
-                    F.unique_squirrel_id = row[2]
-                    F.shift = row[4]
-                    F.date = dt.datetime.strptime(row[5],'%m%d%Y')
-                    F.age = row[7]
-                    F.primary_fur_color = row[8]
-                    F.location = row[12]
-                    F.specific_location = row[14]
-                    F.running = row[15]
-                    F.chasing = row[16]
-                    F.climbing = row[17]
-                    F.eating = row[18]
-                    F.foraging = row[19]
-                    F.other_activities = row[20]
-                    F.kuks = row[21]
-                    F.quuas = row[22]
-                    F.moans = row[23]
-                    F.tail_flags = row[24]
-                    F.tail_twitches = row[25]
-                    F.approaches = row[26]
-                    F.indifferent = row[27]
-                    F.runs_from = row[28]
-
+                    F.Longitude = row[0]
+                    F.Latitude = row[1]
+                    F.Unique_Squirrel_ID = row[2]
+                    F.SHIFT = row[4]
+                    F.Date = dt.datetime.strptime(row[5],'%m%d%Y')
+                    F.AGE = row[7]
+                    F.PRIMARY_FUR_COLOUR = row[8]
+                    F.LOCATION = row[12]
+                    F.Specific_Location = row[14]
+                    F.Running = row[15]
+                    F.Chasing = row[16]
+                    F.Climbing = row[17]
+                    F.Eating = row[18]
+                    F.Foraging = row[19]
+                    F.Other_Activities = row[20]
+                    F.Kuks = row[21]
+                    F.Quuas = row[22]
+                    F.Moans = row[23]
+                    F.Tail_flags = row[24]
+                    F.Tail_twitches = row[25]
+                    F.Approaches = row[26]
+                    F.Indifferent = row[27]
+                    F.Runs_from = row[28]
                 except Fields.DoesNotExist:
                     raise CommandError('there is some error')
-
                 F.save()
