d1fc2298a617b65bf62f135fd3a2b58afbe64319
Aditya Baser <asb2287@columbia.edu>
2019-12-07 21:03:01 +0000

    update urls

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
+from squirrel import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sightings/',views.sighting_list, name='sighting_list'),
