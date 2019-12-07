f837949e84080b61cb6d3c96db0e7538ce75ad5f
Aditya Baser <asb2287@columbia.edu>
2019-12-07 21:00:31 +0000

    update urls

--- project/urls.py
+++ project/urls.py
@@ -18,4 +18,8 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sightings/',views.sighting_list, name='sighting_list'),
+    path('map/',views.sighting_map, name='sighting_map'),
+    path('add/',views.sighting_add, name='sighting_add'),
+    path('', views.index, name='index'),
 ]
