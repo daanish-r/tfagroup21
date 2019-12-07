b8458bfa69a9b7d62119c216f218961f4acb64f8
Aditya Baser <asb2287@columbia.edu>
2019-12-07 21:09:36 +0000

    Update views typo

--- squirrel/views.py
+++ squirrel/views.py
@@ -10,17 +10,17 @@ def index(request):
     return HttpResponse("Hello, this app lets you track squirrels and take a note")
 
 def sighting_list(request, template_name='squirrel/all.html'):
-    sightings = Sighting.objects.all()
+    sightings = Fields.objects.all()
     context = {
             'sightings' : sightings,
     }
     return render(request, template_name, context)
 
 def sighting_map(request, template_name='squirrel/map.html'):
-    sightings = Sighting.objects.order_by('?')[:100]
+    sightings = Fields.objects.order_by('?')[:100]
     context = {
             'sightings' : sightings,
-    
+            }
     return render(request, template_name, context)
 
 def sighting_add(request, template_name='squirrel/add.html'):
