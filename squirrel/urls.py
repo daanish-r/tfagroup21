310d307791ea57b3ad3d38ce5251c8e996e4fced
Aditya Baser <asb2287@columbia.edu>
2019-12-07 05:56:40 +0000

    Update urls

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
             path('sightings/',views.sighting_list, name='sighting_list'),
             path('map/',views.sighting_map, name='sighting_map'),
+            path('add/',views.sighting_add, name='sighting_add'),
             path('', views.index, name='index'),
             ]
